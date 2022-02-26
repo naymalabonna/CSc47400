bfbc45f7899cdc3788b399ee87b6aaa73a475bf3
Nayma <79671237+naymalabonna@users.noreply.github.com>
2022-02-25 19:27:32 -0500

    Create quantitative_vs_categorical.js

---
+++ Assignment 1/quantitative_vs_categorical.js
@@ -0,0 +1,11 @@
+{
+  "width": 800,
+  "height": 400,
+  "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
+  "data": {"url": "https://raw.githubusercontent.com/naymalabonna/CSc47400/main/Assignment%201/imports-85.json"},
+  "mark": "bar",
+  "encoding": {
+    "x": {"field": "make", "type": "nominal"},
+    "y": {"field": "price", "type": "quantitative"}
+  }
+}
