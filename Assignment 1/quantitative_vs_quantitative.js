a0ee7c1bde6fd0398fdb3603db8b7bea1bf61f2d
Nayma <79671237+naymalabonna@users.noreply.github.com>
2022-02-25 19:26:41 -0500

    Update quantitative_vs_quantitative.js

--- Assignment 1/quantitative_vs_quantitative.js
+++ Assignment 1/quantitative_vs_quantitative.js
@@ -2,7 +2,7 @@
   "width": 800,
   "height": 400,
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
-  "data": {"url": "https://raw.githubusercontent.com/naymalabonna/CSc47400/main/imports-85.json"},
+  "data": {"url": "https://raw.githubusercontent.com/naymalabonna/CSc47400/main/Assignment%201/imports-85.json"},
   "mark": "line",
   "encoding": {
     "x": {"field": "price", "type": "quantitative"},
